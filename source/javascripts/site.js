e8200029eb1de03caf88e32cbd9a2aa8834d79bd
rafaela yazawa <yazawrafa@gmail.com>
2025-04-09 20:46:18 +0900

    it's a mess for now. I added a hamburguer menu, and added some media queries

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,7 +1,9 @@
 // This is where it all goes :)
 
 import { initializeMap } from "./event_map.js";
+import { displayMenu } from "./menu_icon.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-	initializeMap();
+  initializeMap();
+  displayMenu();
 });
