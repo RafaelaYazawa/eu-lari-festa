51129ba21ce384672bc89fb56afad547a9835cc3
rafaela yazawa <yazawrafa@gmail.com>
2025-04-16 18:40:30 +0900

    made tabs, added data file for places, created js file for tabs and a css file, did minor modifications in the nav css

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -2,8 +2,10 @@
 
 import { initializeMap } from "./event_map.js";
 import { displayMenu } from "./menu_icon.js";
+import { tabDisplay } from "./tabs.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   initializeMap();
   displayMenu();
+  tabDisplay();
 });
